a70e0d76f2d1bcf07e903fa6444a86746be45c45
Reinis Sestakovskis <reinissestakovskis@gmail.com>
2022-01-21 09:58:45 +0100

    Part 1 done

--- src/model_viewer.cpp
+++ src/model_viewer.cpp
@@ -105,7 +105,7 @@ void do_rendering(Context &ctx)
     cg::reset_gl_render_state();
 
     // Clear color and depth buffers
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     draw_scene(ctx);
